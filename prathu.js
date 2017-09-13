84d6dcae11b404263c00d68c6dd5a6a8341598cb
praitayini <31248960+praitayini@users.noreply.github.com>
2017-09-13 16:04:59 -0400

    Update prathu.js

--- prathu.js
+++ prathu.js
@@ -1,27 +1,19 @@
-document.write(5+6);
-/*jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json',function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-   return items
-  });                  
-                
-   
-   getSParcs2014=function(n){
-    if(!n){n=1000}
-    $.getJSON('https://health.data.ny.gov/resource/pzzw-8zdv.json?$limit='+n)
-       .then(function(data){
-             debugger
-      })*/
 jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
-.then(function(x){
-listHealthdata.dt={data:{}}
-                var div = document.getElementById(id)
-                var h = '<h2 style="color:black">Health Data</h2>'
-                x = x.slice(1).map(function(xi,i){
-                   xi = {rate_per_100_000_people:xi[0],year:xi[1],patient_zipcode:xi[2],pqi_number:xi[3],software_version:xi[4],expected_rate_per_100_000_people:xi[5],pqi_name:xi[6]}
-                   listHealthdata.dt.data[]={rate_per_100_000_people:xi.rate_per_100_000_people,year:xi.year,patient_zipcode:xi.patient_zipcode,pqi_number:xi.pqi_number,software_version:xi.software_version,expected_rate_per_100_000_people:xi.expected_rate_per_100_000_people,pqi_name:xi.pqi_name}
-                   h +='<li id="'+xi.id+'">'+xi.id+': '+xi.name+'</li>'
-                   return xi    
-                })
-   document.write(5+7);
+  .then(function(dt){
+    var avg=function(a){
+      return a.reduce(function(a,b){
+        return a+b
+        })/a.length
+    }
+    console.log('data loaded: ',dt)
+    var x=dt.map(function(xi){
+      return parseFloat(xi.expected_rate_per_100_000_people)
+    })
+    avg(x)
+    // data in x, ready for stats
+    var wk = document.getElementById('work')
+    wk.innerHTML='Number of observartions = '+x.length+'<br>Average value : '+avg(x)
+    wk.style.color='blue'
+
+    //debugger
+})
