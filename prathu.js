ae8bbc3d65feb600e986a9f6dd1488d3fd4c10a8
praitayini <31248960+praitayini@users.noreply.github.com>
2017-09-13 06:43:20 -0400

    Update prathu.js

--- prathu.js
+++ prathu.js
@@ -1,16 +1,11 @@
 document.write(5+6);
-//jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
- function(x){
-                listHealth.dt={study:{}}
-                var div = document.getElementById(id)
-                var h = '<h2 style="color:navy">Health data</h2>'
-                x = x.slice(1).map(function(xi,i){
-                   xi = xi.split(/\t/)
-                   xi = {id:xi[0],name:xi[1],info:xi[2]}
-                   listHealth.dt.study[xi.id]={name:xi.name,info:xi.info}
-                   h +='<li id="'+xi.id+'">'+xi.id+': '+xi.name+'</li>'
-                   return xi                   
-                })
+jQuery.getJSON("https://health.data.ny.gov/resource/5q8c-d6xq.json",function( data ) {
+  var items = [];
+  $.each( data, function( key, val ) {
+    items.push( "<li id='" + key + "'>" + val + "</li>" );
+   return items
+  });                  
+                
    
    getSParcs2014=function(n){
     if(!n){n=1000}
