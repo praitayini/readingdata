303cb2f1d924d66d9ef31d782dd15805156f9c9d
praitayini <31248960+praitayini@users.noreply.github.com>
2017-09-12 21:28:56 -0400

    Update prathu.js

--- prathu.js
+++ prathu.js
@@ -1,8 +1,2 @@
 document.write(5+6);
-jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json', function(data){
-  var items = [];
-  jQuery.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
-  });
-document.write(5+7);
-document.write(items);  
+
