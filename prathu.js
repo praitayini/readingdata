9b4bc6aef112dfcebed3b6085cf1869a08124b10
praitayini <31248960+praitayini@users.noreply.github.com>
2017-09-12 21:51:16 -0400

    Update prathu.js

--- prathu.js
+++ prathu.js
@@ -1,13 +1,13 @@
 document.write(5+6);
-//jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
+jQuery.getJSON('https://health.data.ny.gov/resource/5q8c-d6xq.json')
  function(x){
-                listCancers.dt={study:{}}
+                listHealth.dt={study:{}}
                 var div = document.getElementById(id)
                 var h = '<h2 style="color:navy">Health data</h2>'
                 x = x.slice(1).map(function(xi,i){
                    xi = xi.split(/\t/)
                    xi = {id:xi[0],name:xi[1],info:xi[2]}
-                   listCancers.dt.study[xi.id]={name:xi.name,info:xi.info}
+                   listHealth.dt.study[xi.id]={name:xi.name,info:xi.info}
                    h +='<li id="'+xi.id+'">'+xi.id+': '+xi.name+'</li>'
                    return xi                   
                 })
